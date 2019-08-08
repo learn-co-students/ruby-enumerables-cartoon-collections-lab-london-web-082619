01a004f21309c77093fe3dc909d08b71a8e1c659
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-08 13:42:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index {|dwarves, index| puts "#{index+1}. #{dwarves}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#In the above method you used 'each_with_index' enumerator method to iterate over each element in the dwarf_names array and then asked it to 'puts' a string which places the index +1 infront of each element (which you tagged as 'dwarves' in the block).
+
+
+
+
+
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|calls|
 end
+
+
+
+
+
+
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
 end
+
+
+
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
