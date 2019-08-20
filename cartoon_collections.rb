d648a7a94a963c7aa29740387b31ba712ce94567
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-20 11:35:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarF_names)# code an argument here
+  dwarF_names.each_with_index{|name, i| puts "#{i+1} #{name}" }# Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  planeteer_calls.map{|call| "#{call.capitalize}!"} # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  calls.any? {|call| call.size > 4 } #Your code here
 end
 
 def find_the_cheese# code an argument here
