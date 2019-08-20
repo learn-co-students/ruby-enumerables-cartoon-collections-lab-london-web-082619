7a38c150837082f9653ad0a786df79c9896252b2
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-20 12:51:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ end
 
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
-
   potentially_cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   end
